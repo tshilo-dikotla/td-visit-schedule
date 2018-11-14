63df5ed36efe031f58818c27e63caa2e540a21d9
Leslie <leslie.kep@gmail.com>
2018-11-14 09:41:39 +0200

    update schedule

--- td_maternal_visit_schedule/visit_schedules/antenatal_visit_schedule.py
+++ td_maternal_visit_schedule/visit_schedules/antenatal_visit_schedule.py
@@ -1,17 +1,20 @@
 from edc_visit_schedule import Schedule, Visit
 from dateutil.relativedelta import relativedelta
 
-from .crfs import crf_1000
+from .maternal_requisitions import requisitions_1010m, requisitions_1020m
+from .crfs import crf_1000, crf_1010, crf_1020
+
+# TODO: Add PRN and Lab Requisitions for visits.
 
 schedule = Schedule(
     name='schedule',
     verbose_name='Day 1 to 36 months Follow-up',
     onschedule_model='td_maternal.onschedule',
-    offschedule_model='td_maternal.studyterminationconclusion',
+    offschedule_model='td_maternal.maternaloffstudy',
     consent_model='td_maternal.maternalconsent',
     appointment_model='edc_appointment.appointment')
 
-visit0 = Visit(
+visit1000 = Visit(
     code='1000M',
     title='Antenatal Enrollment',
     timepoint=0,
@@ -20,32 +23,30 @@ visit0 = Visit(
     rupper=relativedelta(days=0),
     requisitions=None,
     crfs=crf_1000,
-    #     requisitions_prn=FormsCollection(
-    #         *[r for r in default_requisitions_prn if r.panel != chemistry_panel]),
     facility_name='5-day clinic')
 
-visit1 = Visit(
+visit1010 = Visit(
     code='1010M',
     title='Antenatal Visit 1',
     timepoint=1,
     rbase=relativedelta(days=1),
     rlower=relativedelta(days=0),
     rupper=relativedelta(days=0),
-    requisitions=None,
-    crfs=crf_1000,
-    #     requisitions_prn=FormsCollection(
-    #         *[r for r in default_requisitions_prn if r.panel != chemistry_panel]),
+    requisitions=requisitions_1010m,
+    crfs=crf_1010,
     facility_name='5-day clinic')
 
-visit2 = Visit(
+visit1020 = Visit(
     code='1020M',
     title='Antenatal Visit 2',
-    timepoint=1,
+    timepoint=2,
     rbase=relativedelta(days=3),
     rlower=relativedelta(days=0),
     rupper=relativedelta(days=0),
-    requisitions=None,
-    crfs=crf_1000,
-    #     requisitions_prn=FormsCollection(
-    #         *[r for r in default_requisitions_prn if r.panel != chemistry_panel]),
+    requisitions=requisitions_1020m,
+    crfs=crf_1020,
     facility_name='5-day clinic')
+
+schedule.add_visit(visit=visit1000)
+schedule.add_visit(visit=visit1010)
+schedule.add_visit(visit=visit1020)
---
+++ td_maternal_visit_schedule/visit_schedules/maternal_requisitions.py
@@ -0,0 +1,65 @@
+from edc_visit_schedule import FormsCollection, Requisition
+from td_labs import cd4_panel, viral_load_panel, pbmc_vl_panel
+from td_labs import fasting_glucose_panel, glucose_1h_panel, glucose_2h_panel
+from td_labs import pbmc_pl_panel, elisa_panel, insulin_panel
+
+
+requisitions_1010m = FormsCollection(
+    Requisition(
+        show_order=10,
+        panel=cd4_panel, required=False, additional=True),
+    Requisition(
+        show_order=20,
+        panel=pbmc_vl_panel, required=False, additional=True),
+    Requisition(
+        show_order=30,
+        panel=fasting_glucose_panel, required=True, additional=True),
+    Requisition(
+        show_order=40,
+        panel=glucose_1h_panel, required=True, additional=True),
+    Requisition(
+        show_order=50,
+        panel=glucose_2h_panel, required=True, additional=True),
+    Requisition(
+        show_order=60,
+        panel=pbmc_pl_panel, required=False, additional=True),
+    Requisition(
+        show_order=70,
+        panel=viral_load_panel, required=False, additional=True),
+)
+
+requisitions_1020m = FormsCollection(
+    Requisition(
+        show_order=10,
+        panel=viral_load_panel, required=False, additional=True),
+    Requisition(
+        show_order=20,
+        panel=cd4_panel, required=False, additional=True),
+    Requisition(
+        show_order=30,
+        panel=fasting_glucose_panel, required=False, additional=True),
+    Requisition(
+        show_order=40,
+        panel=glucose_1h_panel, required=False, additional=True),
+    Requisition(
+        show_order=50,
+        panel=glucose_2h_panel, required=False, additional=True),
+)
+
+requisitions_followup = FormsCollection(
+    Requisition(
+        show_order=10,
+        panel=viral_load_panel, required=False, additional=True),
+    Requisition(
+        show_order=20,
+        panel=cd4_panel, required=False, additional=True),
+    Requisition(
+        show_order=30,
+        panel=fasting_glucose_panel, required=False, additional=True),
+    Requisition(
+        show_order=40,
+        panel=glucose_1h_panel, required=False, additional=True),
+    Requisition(
+        show_order=50,
+        panel=glucose_2h_panel, required=False, additional=True),
+)
