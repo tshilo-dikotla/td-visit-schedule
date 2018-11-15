0a3ee81173e463c21df9e14da9b2cd359ab3abe8
moffatmore <mofenyimoffat@gmail.com>
2018-11-15 16:32:14 +0200

    fixing infant crfs adding required & additional

--- td_visit_schedule/visit_schedules/infant_crfs.py
+++ td_visit_schedule/visit_schedules/infant_crfs.py
@@ -7,9 +7,11 @@ crf_2000 = FormsCollection(
     Crf(show_order=3, model='td_infant.infantbirthfeedingvaccine'),
     Crf(show_order=4, model='td_infant.infantbirtharv'),
     Crf(show_order=5, model='td_infant.infantcongenitalanomalies'),
-    Crf(show_order=6, model='td_infant.infantdeathreport'),
-    Crf(show_order=7, model='td_infant.infantnvpdispensing'),
-    name='Birth v3'
+    Crf(show_order=6, model='td_infant.infantdeathreport', required=False,
+        additional=True),
+    Crf(show_order=7, model='td_infant.infantnvpdispensing', required=False,
+        additional=True),
+    name='Birth'
 )
 
 crf_2010 = FormsCollection(
@@ -21,7 +23,7 @@ crf_2010 = FormsCollection(
     Crf(show_order=6, model='td_infant.infantfeeding'),
     Crf(show_order=7, model='td_infant.infantnvpadjustment'),
     Crf(show_order=8, model='td_infant.infantfuimmunizations'),
-    name='Infant 1 Month Visit v3'
+    name='Infant 1 Month Visit'
 )
 
 crf_2020 = FormsCollection(
@@ -32,7 +34,18 @@ crf_2020 = FormsCollection(
     Crf(show_order=5, model='td_infant.infantfuimmunizations'),
     Crf(show_order=6, model='td_infant.infantarvproph'),
     Crf(show_order=7, model='td_infant.infantfeeding'),
-    name='Infant 6 Month Visit v3'
+    name='Infant 2 Month Visit '
+)
+
+crf_2060 = FormsCollection(
+    Crf(show_order=1, model='td_infant.infantfu'),
+    Crf(show_order=2, model='td_infant.infantfuphysical'),
+    Crf(show_order=3, model='td_infant.infantfudx'),
+    Crf(show_order=4, model='td_infant.infantfunewmed'),
+    Crf(show_order=5, model='td_infant.infantfeeding'),
+    Crf(show_order=5, model='td_infant.solidfoodassessment'),
+    Crf(show_order=6, model='td_infant.infantfuimmunizations'),
+    name='Infant 6 Month Visit '
 )
 
 crf_2120 = FormsCollection(
@@ -43,7 +56,7 @@ crf_2120 = FormsCollection(
     Crf(show_order=5, model='td_infant.infantfeeding'),
     Crf(show_order=5, model='td_infant.solidfoodassessment'),
     Crf(show_order=6, model='td_infant.infantfuimmunizations'),
-    name='Infant 12 Month Visit v3'
+    name='Infant 12 Month Visit'
 )
 
 crf_2180 = FormsCollection(
@@ -54,7 +67,7 @@ crf_2180 = FormsCollection(
     Crf(show_order=5, model='td_infant.infantfeeding'),
     Crf(show_order=5, model='td_infant.solidfoodassessment'),
     Crf(show_order=6, model='td_infant.infantfuimmunizations'),
-    name='Infant 18 Month Visit v3'
+    name='Infant 18 Month Visit'
 )
 
 crf_2240 = FormsCollection(
@@ -65,7 +78,7 @@ crf_2240 = FormsCollection(
     Crf(show_order=5, model='td_infant.infantfeeding'),
     Crf(show_order=5, model='td_infant.solidfoodassessment'),
     Crf(show_order=6, model='td_infant.infantfuimmunizations'),
-    name='Infant 24 Month Visit v3'
+    name='Infant 24 Month Visit'
 )
 
 crf_2300 = FormsCollection(
@@ -76,7 +89,7 @@ crf_2300 = FormsCollection(
     Crf(show_order=5, model='td_infant.infantfeeding'),
     Crf(show_order=5, model='td_infant.solidfoodassessment'),
     Crf(show_order=6, model='td_infant.infantfuimmunizations'),
-    name='Infant 30 Month Visit v3'
+    name='Infant 30 Month Visit'
 )
 
 crf_2360 = FormsCollection(
@@ -87,5 +100,5 @@ crf_2360 = FormsCollection(
     Crf(show_order=5, model='td_infant.infantfeeding'),
     Crf(show_order=5, model='td_infant.solidfoodassessment'),
     Crf(show_order=6, model='td_infant.infantfuimmunizations'),
-    name='Infant 36 Month Visit v3'
+    name='Infant 36 Month Visit'
 )
