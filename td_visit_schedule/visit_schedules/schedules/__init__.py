5671c6ba0244fd2aa5302b34e225b82f4690fe80
Ame Diphoko <diphokoame@gmail.com>
2018-11-26 10:57:53 +0200

    fixing antenatal membership visit schedule v1

--- td_visit_schedule/visit_schedules/schedules/__init__.py
+++ td_visit_schedule/visit_schedules/schedules/__init__.py
@@ -3,3 +3,4 @@ from .maternal import (
     maternal_labour_del_schedule_v1
 )
 from .maternal import antenatal_membership_schedule_v3
+from .maternal import antenatal_membership_schedule_1
--- td_visit_schedule/visit_schedules/visit_schedule.py
+++ td_visit_schedule/visit_schedules/visit_schedule.py
@@ -1,4 +1,6 @@
 from edc_visit_schedule import VisitSchedule, site_visit_schedules
+from .schedules.maternal import antenatal_membership_schedule_1
+
 from .schedules import antenatal_schedule_1, antenatal_schedule_3
 from .schedules.maternal import antenatal_membership_schedule_v3
 
@@ -47,17 +49,19 @@ antenatal_membership_visit_schedule_v3 = VisitSchedule(
 antenatal_membership_visit_schedule_v3.add_schedule(
     antenatal_membership_schedule_v3)
 
-maternal_enrollment_visit_v3 = VisitSchedule(
-    name='maternal_enrollment_visit_schedule_v3',
-    verbose_name='Maternal Enrollment Visit Schedule v3',
-    offstudy_model=f'td_maternal.maternaloffstudy',
-    death_report_model=f'',
-    locator_model='edc_locator.subjectlocator',
+antenatal_membership_visit_schedule_v1 = VisitSchedule(
+    name='antenatal_membership_visit_schedule_v1',
+    verbose_name='Antenatal Visit Membership Schedule 1',
+    offstudy_model='td_maternal.maternaloffstudy',
+    death_report_model='td_maternal.deathreport',
+    locator_model='edc_report.subjectlocator',
     previous_visit_schedule=None
 )
-maternal_enrollment_visit_v3.add_schedule(maternal_enrollment_visit_v3)
+antenatal_membership_visit_schedule_v1.add_schedule(
+    antenatal_membership_schedule_1)
 
 site_visit_schedules.register(antenatal_visit_schedule_v1)
 site_visit_schedules.register(antenatal_visit_schedule_v3)
 site_visit_schedules.register(maternal_labour_visit_schedule_v1)
 site_visit_schedules.register(antenatal_membership_visit_schedule_v3)
+site_visit_schedules.register(antenatal_membership_visit_schedule_v1)
