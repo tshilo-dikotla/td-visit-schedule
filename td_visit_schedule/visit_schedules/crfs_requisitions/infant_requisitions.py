3e306b060718c168bee7d9aa274aef795a6cf9b5
MoffatMore <mofenyimoffat@gmail.com>
2019-06-21 11:21:53 +0200

    adding infant pbmc/pl at visit 2010

--- td_visit_schedule/visit_schedules/crfs_requisitions/infant_requisitions.py
+++ td_visit_schedule/visit_schedules/crfs_requisitions/infant_requisitions.py
@@ -45,11 +45,13 @@ infant_1month_requisitions = FormsCollection(
     Requisition(
         show_order=30, panel=dna_pcr, required=True, additional=True),
     Requisition(
-        show_order=40, panel=serum_panel, required=True, additional=True),
+        show_order=40, panel=infant_pbmc_pl_panel, required=True, additional=True),
     Requisition(
-        show_order=50, panel=dbs_panel, required=False, additional=True),
+        show_order=50, panel=serum_panel, required=True, additional=True),
     Requisition(
-        show_order=60, panel=infant_paxgene_panel, required=False, additional=True),
+        show_order=60, panel=dbs_panel, required=False, additional=True),
+    Requisition(
+        show_order=70, panel=infant_paxgene_panel, required=False, additional=True),
     name='requisitions_2010'
 )
 
@@ -86,7 +88,6 @@ infant_18month_requisitions = FormsCollection(
         show_order=40, panel=infant_pbmc_pl_panel, required=True, additional=True),
     Requisition(
         show_order=50, panel=karabo_pbmc_pl_panel, required=False, additional=True),
-
     name='requisitions_2180'
 )
 
