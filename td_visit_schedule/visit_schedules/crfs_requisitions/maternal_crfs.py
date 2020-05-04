95a079d6e1701bd610a069fd7d74dd166eb092c5
ame diphoko <diphokoame@gmail.com>
2020-05-04 09:54:23 +0200

    update clinician notes on maternal crfs

--- td_visit_schedule/visit_schedules/crfs_requisitions/maternal_crfs.py
+++ td_visit_schedule/visit_schedules/crfs_requisitions/maternal_crfs.py
@@ -19,6 +19,8 @@ crf_1000 = FormsCollection(
     Crf(show_order=7, model='td_maternal.maternalclinicalmeasurementsone',
         required=False),
     Crf(show_order=8, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=9, model='td_maternal.cliniciannotes',
+        required=False),
     name='enrollment')
 
 crf_1010 = FormsCollection(
@@ -27,6 +29,8 @@ crf_1010 = FormsCollection(
     Crf(show_order=3, model='td_maternal.maternalclinicalmeasurementstwo'),
     Crf(show_order=4, model='td_maternal.rapidtestresult', required=False),
     Crf(show_order=5, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=6, model='td_maternal.cliniciannotes',
+        required=False),
     name='antenatal1')
 
 crf_1020 = FormsCollection(
@@ -37,6 +41,8 @@ crf_1020 = FormsCollection(
     Crf(show_order=5, model='td_maternal.maternalclinicalmeasurementstwo'),
     Crf(show_order=6, model='td_maternal.rapidtestresult', required=False),
     Crf(show_order=7, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=8, model='td_maternal.cliniciannotes',
+        required=False),
     name='antenatal2')
 
 crf_2000 = FormsCollection(
@@ -48,6 +54,8 @@ crf_2000 = FormsCollection(
     Crf(show_order=5, model='td_maternal.maternalsubstanceuseduringpreg'),
     Crf(show_order=6, model='td_maternal.rapidtestresult', required=False),
     Crf(show_order=7, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=8, model='td_maternal.cliniciannotes',
+        required=False),
     name='birth')
 
 crf_2010 = FormsCollection(
@@ -59,6 +67,8 @@ crf_2010 = FormsCollection(
     Crf(show_order=6, model='td_maternal.maternalclinicalmeasurementstwo'),
     Crf(show_order=7, model='td_maternal.maternalcontraception', required=False),
     Crf(show_order=8, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=9, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup1')
 
 crf_2020 = FormsCollection(
@@ -72,6 +82,8 @@ crf_2020 = FormsCollection(
     Crf(show_order=8, model='td_maternal.maternalcontraception'),
     Crf(show_order=9, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=10, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=11, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup2')
 
 crf_2060 = FormsCollection(
@@ -85,6 +97,8 @@ crf_2060 = FormsCollection(
     Crf(show_order=8, model='td_maternal.maternalcontraception'),
     Crf(show_order=9, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=10, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=11, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup3')
 
 crf_2120 = FormsCollection(
@@ -99,6 +113,8 @@ crf_2120 = FormsCollection(
     Crf(show_order=9, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=10, model='td_maternal.maternalrecontact'),
     Crf(show_order=11, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=12, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup4')
 
 crf_2180 = FormsCollection(
@@ -111,6 +127,8 @@ crf_2180 = FormsCollection(
     Crf(show_order=7, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=8, model='td_maternal.maternalrecontact'),
     Crf(show_order=9, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=10, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup5')
 
 crf_2240 = FormsCollection(
@@ -123,6 +141,8 @@ crf_2240 = FormsCollection(
     Crf(show_order=7, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=8, model='td_maternal.maternalrecontact'),
     Crf(show_order=9, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=10, model='td_maternal.cliniciannotes',
+        required=False),
     name='follow6')
 
 crf_2300 = FormsCollection(
@@ -135,6 +155,8 @@ crf_2300 = FormsCollection(
     Crf(show_order=7, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=8, model='td_maternal.maternalrecontact'),
     Crf(show_order=9, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=10, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup7')
 
 crf_2360 = FormsCollection(
@@ -147,4 +169,6 @@ crf_2360 = FormsCollection(
     Crf(show_order=7, model='td_maternal.maternalsrh', required=False),
     Crf(show_order=8, model='td_maternal.maternalrecontact'),
     Crf(show_order=9, model='td_maternal.maternalfoodsecurity'),
+    Crf(show_order=10, model='td_maternal.cliniciannotes',
+        required=False),
     name='followup8')
